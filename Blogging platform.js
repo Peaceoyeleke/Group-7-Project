627a5edc61ccdb3fb87b5bf53435fc137a9d663a
Peace Oyeleke <oyelekept@gmail.com>
2024-04-05 17:58:30 +0100

    my projects

--- Blogging platform.js
+++ Blogging platform.js
@@ -77,30 +77,3 @@ function authenticate(username, password) {
     }
 }
 
-function openModal() {
-    modal.style.display = 'block';
-    postTitleInput.value = '';
-    postContentTextarea.value = '';
-}
-
-function closeModal() {
-    modal.style.display = 'none';
-}
-
-function savePost() {
-    const title = postTitleInput.value;
-    const content = postContentTextarea.value;
-    if (title && content) {
-        const post = document.createElement('div');
-        post.classList.add('post');
-        post.innerHTML = `<h2>${title}</h2><p>${content}</p>`;
-        postList.appendChild(post);
-        closeModal();
-    } else {
-        alert('Please enter title and content');
-    }
-}
-
-closeButton.addEventListener('click', closeModal);
-newPostButton.addEventListener('click', openModal);
-savePostButton.addEventListener('click', savePost);
